f3345011629c0ac46cd0155992ec4aa1c76de1f3
Sankalp verma <43778681+sanki4489@users.noreply.github.com>
2022-10-12 17:37:49 +0900

    Update api.js

--- api.js
+++ api.js
@@ -78,3 +78,36 @@ fetch(api, {
 //     }
 //   },
 // }
+
+
+
+
+
+// import Vue from 'vue'
+// export default Vue.extend({
+//   mounted() {
+//     // const content = document?.getElementById('iframesrc')
+//     const api = 'https://api-dev.conecame.com/v1/view/18'
+//     const token = `eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiOTFhYTlkOGI4ODA0NjdmNDA4ZGJiMWYxZjViMDg4YmI5NzhlZjU2MWFlNTllMDkxYzQ1YTBlZWVkM2RiMzk3MGE4NmZhMjQwZTYxNzdlZWIiLCJpYXQiOjE2NjU1MzI4MTQuNzEwMzQsIm5iZiI6MTY2NTUzMjgxNC43MTAzNDYsImV4cCI6MTY2NTYxOTIxNC41NzcwNzEsInN1YiI6IjIiLCJzY29wZXMiOltdfQ.iMBcrB8th45QDzMJpIgpcoeA_sa3DY8ZCY1ycTblgxw2gYQD8DoMBuuxSwHQI5JnsL-VqS4FyCmfCrS3m4M4yeLFOARzKUMwPFvgwOUQ6H8sIOv6GxYMxIiaYmYg6I1WsVVZzaryS12bUjbIT-Y8DGfqipfY2-DUuHYZK5Xe0izXyGV9UXPXNg2LxVU7c7ToP2CBHZ9dUePmjFL7vHt3hIEqYrr5zb8y3YC3jQEhHv3CQrGMg2CeTXywdGwTJui4lZxhsL3J1cBh5HRlz2pb3UEijiV36I81pC12GPJamUfEoF1vBxi2Y8zSx9TKYj_XoDcJs1EsH-g3yMVACrcVbwWxxVNELAG-fERFhPHdEPkyV-AWu-hAMJdGcCRE48hQvxD8MB0hj671uhS5FK725QVfJwVC6c8dkzgub_Q3yuZ34sJkHfwBf7J2Wjrc7wz2m-uj5axvEXPqyUkF6fv6I9NRaS2NN1FJz5BiecDFKe9fJ9j6TuTjLgOZsLwOVT-dZMWEmliT1AxbZ-zXo36bt2wNHsimQTRzhIL6JgHSvZrRxEcdmWIUgUwEyDbtOzHbhiDEaowSqIas6h4VFFtJYzru39DbNua4CE47za0PGfA8j1KCGXzf12wGCdYYHxHOvNE8V_8Zrimk4mCIkWmmfQDlyUhq-xzTpN53yzoBaEs`
+//     fetch(api, {
+//       headers: {
+//         'Content-Type': `application/json`,
+//         Authorization: `Bearer ${token}`,
+//       },
+//     })
+//       .then((res) => {
+//         if (res.ok) {
+//           return res.text()
+//         }
+//       })
+//       .then((data) => {
+//         const content = document?.querySelector(
+//           '#iframesrc'
+//         ) as HTMLIFrameElement
+//         content.contentWindow?.document.open()
+//         content.contentWindow?.document.write(data)
+//         content.contentWindow?.document.close()
+//       })
+//   },
+// })
+
